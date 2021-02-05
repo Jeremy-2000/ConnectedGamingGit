4b4893f3765af9fd2690b63e249614d475fdd24c
Jeremy-2000 <63964628+Jeremy-2000@users.noreply.github.com>
2021-02-05 21:40:07 +0100

    Username & Firebase Login
    Player needs to enter username to be able to click on the buttons to play chess.
    Added Firebase to the project with login and be initialized.

---
+++ Assets/Scripts/FirebaseScript.cs
@@ -0,0 +1,116 @@
+﻿using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Storage;
+using Firebase.Extensions;
+using Firebase.Unity.Editor;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Threading;
+using System.IO;
+
+public class FirebaseScript : MonoBehaviour
+{
+    public bool signedin = false;
+    DatabaseReference reference;
+
+    //reference to the storage bucket
+    FirebaseStorage storage;
+
+    //main data dictionary
+    Dictionary<string, object> myDataDictionary;
+
+    FirebaseAuth auth;
+
+    //string email = "practical@gmail.com";
+    //string password = "Test!2345";
+
+    string email = "chess@gmail.com";
+    string password = "123456";
+
+    string path = "https://jeremyattardunity.firebaseio.com/";
+
+    public bool isDownloaded;
+
+    public IEnumerator clearFirebase()
+    {
+        Task removeAllRecords = reference.RemoveValueAsync().ContinueWithOnMainThread(
+            rmAllRecords =>
+            {
+                if (rmAllRecords.IsCompleted)
+                {
+                    Debug.Log("Database clear");
+                }
+            });
+
+        yield return new WaitUntil(() => removeAllRecords.IsCompleted);
+
+    }
+
+    public IEnumerator initFirebase(string path)
+    {
+        if (!signedin)
+        {
+            FirebaseApp.DefaultInstance.SetEditorDatabaseUrl(path);
+            reference = FirebaseDatabase.DefaultInstance.RootReference;
+            yield return signInToFirebase();
+            Debug.Log("Firebase Initialized!");
+            yield return true;
+            signedin = true;
+        }
+        else
+        {
+            yield return null;
+        }
+    }
+
+    IEnumerator signInToFirebase()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+
+        //the outside task is a DIFFERENT NAME to the anonymous inner class
+        Task signintask = auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(
+             signInTask =>
+             {
+                 if (signInTask.IsCanceled)
+                 {
+                     //write cancelled in the console
+                     Debug.Log("Cancelled!");
+                     return;
+                 }
+                 if (signInTask.IsFaulted)
+                 {
+                     //write the actual exception in the console
+                     Debug.Log("Something went wrong!" + signInTask.Exception);
+                     return;
+                 }
+
+                 Firebase.Auth.FirebaseUser loggedInUser = signInTask.Result;
+                 Debug.Log("User " + loggedInUser.DisplayName + " has logged in!");
+             }
+            );
+        yield return new WaitUntil(() => signintask.IsCompleted);
+
+        Debug.Log("User has signed in");
+    }
+
+    private Sprite LoadSprite(string path)
+    {
+        if (string.IsNullOrEmpty(path)) return null;
+        if (System.IO.File.Exists(path))
+        {
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+            Texture2D texture = new Texture2D(1, 1);
+            texture.LoadImage(bytes);
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+            return sprite;
+        }
+        return null;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,26 +9,77 @@ public class GameManager : MonoBehaviour
 {
     public InputField username;
     public Button Set2D, Set3D;
+    public FirebaseScript db;
 
-    // Start is called before the first frame update
-    void Start()
+    string path = "https://jeremyattardunity.firebaseio.com/";
+
+    void Awake()
     {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("MainCamera");
 
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
     }
 
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+
+        Camera.main.gameObject.AddComponent<FirebaseScript>();
+        db = Camera.main.GetComponent<FirebaseScript>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "MainMenu")
+        {
+            try
+            {
+                if (username.text != "")
+                {
+                    Set2D.GetComponent<Button>().interactable = true;
+                    Set3D.GetComponent<Button>().interactable = true;
+                }
+
+                else
+                {
+                    Set2D.GetComponent<Button>().interactable = false;
+                    Set3D.GetComponent<Button>().interactable = false;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
     }
 
     public void LoadGameScene()
     {
+        StartCoroutine(initDB(path));
         SceneManager.LoadScene("Game");
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    IEnumerator clearDB()
+    {
+        yield return db.clearFirebase();
+        Application.Quit();
+    }
+
+    IEnumerator initDB(string path)
+    {
+        yield return db.initFirebase(path);
     }
 }
